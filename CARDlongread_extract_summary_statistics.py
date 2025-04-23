8f2f847fb6d10cd2ac975b4dd43eb3c181c09f15
Abraham Moller <mollerabg@biowulf.nih.gov>
2025-04-23 01:35:26 -0400

    Fixed mpatches library import

--- CARDlongread_extract_summary_statistics.py
+++ CARDlongread_extract_summary_statistics.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import statistics
 import argparse
 import dataclasses
